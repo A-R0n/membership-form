28e11fc38718f381602ab10f01ed583c1af7fbc0
Aaron Estes <aestesc@gmail.com>
2019-05-08 13:10:02 -0500

    Update variable name

--- src/server/index.php
+++ src/server/index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 
-if (empty($_POST['fname']) && empty($_POST['email'])) die();
+if (empty($_POST['name']) && empty($_POST['email'])) die();
 
 if ($_POST)
 	{
